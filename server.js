12f72f59ced69cdfbc6be323fbd3e91dcc0a4f99
ridvan saman <ridvansaman@unknown-f4:d4:88:79:37:9e.lan>
2022-01-20 14:45:26 +0000

    routes added with express.Router

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./routes');
+
 const app = express();
 
 const port = 3000
+
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname, './views'))
+
 app.use(express.static(path.join(__dirname, './static')))
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './static/index.html'))
-})
-app.get('/speakers', (req, res) => {
-    res.sendFile(path.join(__dirname, './static/speakers.html'))
-})
+
+app.use('/',routes());
 
 app.listen(port,()=> {
-    console.log(`Express server is listening on ${port}`);
+     console.log(`Express server is listening on ${port}`);
 })
